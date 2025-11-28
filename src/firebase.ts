48aabfcfc79dac1619cd8a29fb928be4977a7a0d
TagFri <115897998+TagFri@users.noreply.github.com>
2025-11-28 12:12:48 +0100

    Move Firebase config to env vars

--- src/firebase.ts
+++ src/firebase.ts
@@ -2,17 +2,15 @@ import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 
-// BYTT UT MED DIN CONFIG FRA FIREBASE CONSOLE
 const firebaseConfig = {
-    apiKey: "AIzaSyARwb6K0M4l-hWKi8FrOLEVqRqeHRf8JR4",
-    authDomain: "oppmoteboka.firebaseapp.com",
-    projectId: "oppmoteboka",
-    storageBucket: "oppmoteboka.firebasestorage.app",
-    messagingSenderId: "732911922070",
-    appId: "1:732911922070:web:b16d9de65062395c018bb7"
+    apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+    authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+    projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+    storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+    messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+    appId: import.meta.env.VITE_FIREBASE_APP_ID,
 };
 
 const app = initializeApp(firebaseConfig);
-
 export const auth = getAuth(app);
 export const db = getFirestore(app);
