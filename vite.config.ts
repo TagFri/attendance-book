966adb29d3a1f57b6fd59f0f35e22d2955ad5c16
TagFri <115897998+TagFri@users.noreply.github.com>
2025-12-01 12:17:46 +0100

    vite config change

--- vite.config.ts
+++ vite.config.ts
@@ -3,5 +3,5 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/attendance-book/',   // 👈 MÅ matche repo-navnet
+  base: '/',   // 👈 MÅ matche repo-navnet
 })
